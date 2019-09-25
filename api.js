670812274e165e569272ea87f0fc2a471f5a452d
pablopose <posepablo7@gmail.com>
2019-09-24 21:59:47 -0300

    Agregando LokiJS para almacenamiento de datos

--- api.js
+++ api.js
@@ -1,21 +1,43 @@
+/* Modulos / Modules */
 const express = require("express")
 const bodyParser = require("body-parser")
-const server = express()
+const loki = require("lokijs")
 
+/* Auxiliares / Helpers */
+const server = express()
 const port = 2000
 const header = { "Content-Type" : "application/json; charset=utf-8"} 
 
+let personas = null
+
+const db = new loki("datos.json", {
+	autoload : true, /*true carga, false cada vez que se resentea*/ 
+	autosave : true /*true autosave, false no*/,
+	autosaveInterval : 5000,
+	autoloadCallback : () => {
+
+		personas = db.addCollection("personas") || db.getCollection("personas")
+	}
+})
+
+/* Configurarciones / Configurations */
 server.listen( port )
 server.use( bodyParser.urlencoded({ extended : false }))
 server.use( bodyParser.json() )
 
+/* Procesos / Process */
 server.get("/api", (req, res) => {
+
 	res.set( header )
-	res.end("Aca voy a devolver datos en formato JSON")
+	res.json( personas.data )
 })
 
 server.post("/api", (req, res) => {
+
+	let persona = req.body
+
+	personas.insert(persona)
+
 	res.set( header )
-	//console.log( req.body )
-	res.json( req.body )
+	res.json( { "rta" : "ok" } )
 })
